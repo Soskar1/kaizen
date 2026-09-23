73d5ba65471e59304d33356f455384100da60b86
oskar <oskar.vistorskis@gmail.com>
2026-09-23 20:50:36 +0300

    Implemented GET /activities

--- crates/kaizen-api/src/main.rs
+++ crates/kaizen-api/src/main.rs
@@ -1,8 +1,6 @@
-use std::{env, fs};
-use axum::{
-    routing::get,
-    Router,
-};
+use std::{env, fs, io};
+use std::path::Path;
+use axum::{routing::get, Router, Json};
 use axum::http::StatusCode;
 
 #[tokio::main]
@@ -14,8 +12,34 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_activities() {
+async fn get_activities() -> Result<Json<Vec<String>>, StatusCode> {
+    let path = env::current_dir().map_err(|error| {
+        eprintln!("Failed to get current directory: {:?}", error);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+    
+    let activities = read_activity_names(&path).map_err(|error|{
+        eprintln!("Failed to read activities: {:?}", error);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
 
+    Ok(Json(activities))
+}
+
+fn read_activity_names(path: &Path) -> io::Result<Vec<String>> {
+    let paths = fs::read_dir(path)?;
+
+    let mut activities: Vec<String> = vec!();
+
+    for path in paths {
+        let path = path?.path();
+
+        if path.is_dir() {
+            activities.push(path.file_name().unwrap().to_str().unwrap().to_string());
+        }
+    }
+
+    Ok(activities)
 }
 
 async fn post_activities(body: String) -> StatusCode {
@@ -26,4 +50,42 @@ async fn post_activities(body: String) -> StatusCode {
     fs::create_dir(format!("C:\\Projects\\Rust\\{name}")).unwrap();
 
     StatusCode::CREATED
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use tempfile::tempdir;
+    use super::*;
+
+    #[rstest]
+    #[case(vec!("Rust", "Learning"))]
+    #[case(vec!())]
+    fn read_activity_names_must_return_all_activity_names(#[case] directories_to_create: Vec<&str>) {
+        // Arrange
+        let temp_dir = tempdir().unwrap();
+        let temp_dir_path = temp_dir.path();
+
+        for directory_name in directories_to_create.clone() {
+            let directory_to_create = temp_dir_path.join(directory_name);
+            fs::create_dir(directory_to_create).unwrap();
+        }
+
+        // Act
+        let activity_names = read_activity_names(temp_dir_path).unwrap();
+
+        // Assert
+        let expected = directories_to_create
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<_>>();
+
+        for name in activity_names.clone() {
+            assert!(expected.contains(&name));
+        }
+
+        for expected_name in expected {
+            assert!(activity_names.contains(&expected_name));
+        }
+    }
 }
